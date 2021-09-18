d123d44af410acd96b3a82d426d64b8e0ad2dc24
mricarte <mricarte@itba.edu.ar>
2021-09-18 17:14:57 -0300

    BGM for finish screen

--- scenes/menus/FinishScreen.gd
+++ scenes/menus/FinishScreen.gd
@@ -2,6 +2,8 @@ extends Control
 
 
 onready var timeLabel = $Label
+onready var anim = $AnimationPlayer
+onready var audio = $AudioStreamPlayer
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -12,12 +14,19 @@ func show_result(condition):
 	self.visible = true
 	var time = Checkpoint.get_time()
 	timeLabel.text = time
+	
 	if condition:
-		$VBoxContainer/CenterContainer/great_text.visible = true
-		$VBoxContainer/CenterContainer/bad_text.visible = false
+		anim.play("show_result_good")
 	else:
-		$VBoxContainer/CenterContainer/great_text.visible = false
-		$VBoxContainer/CenterContainer/bad_text.visible = true
+		anim.play("show_result_bad")
+	
+	#if condition:
+	#	$VBoxContainer/CenterContainer/great_text.visible = true
+	#	$VBoxContainer/CenterContainer/bad_text.visible = false
+	#else:
+	#	$VBoxContainer/CenterContainer/great_text.visible = false
+	#	$VBoxContainer/CenterContainer/bad_text.visible = true
+	
 	#timeLabel.bbcode_text = "[wave amp=50 freq=5][center] %s [/center][/wave]" % time
 
 func hide() -> void:
@@ -37,3 +46,7 @@ func _on_NextButton_pressed() -> void:
 func _on_ExitButton_pressed() -> void:
 	Checkpoint.clean_up()
 	get_tree().change_scene("res://scenes/menus/MainMenu.tscn")
+
+
+func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
+	audio._set_playing(false)
